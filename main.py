958bbcac84df96710e646e7ba630fe0d06ef40b9
Diego Herrera <diego.herrera@ues.edu.sv>
2024-05-21 15:08:30 -0600

    Agregado un test llamado: calculate age

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ def say_hello(name):
     print(f'Hello, {name}')  # Press ⌘F8 to toggle the breakpoint.
 
 def calculate_age():
-    return True
+    return False
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
